83b38c485537adf1ae3b51ab66186a4c27b8d197
Hector Hidalgo <marce.753@gmail.com>
2021-11-16 08:52:11 -0400

    [LSB-51] Create script for dictation usage

---
+++ Assets/DictationManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using FantomLib;
+using UnityEngine.Events;
+using UnityEngine.Android;
+
+public class DictationManager : MonoBehaviour
+{
+    [Serializable] public class ResultHandler : UnityEvent<string> { }
+    public ResultHandler OnRequest;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RequestUserMicrophonePermission();
+    }
+
+    private static void RequestUserMicrophonePermission()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+    }
+
+    public void SwitchWebSearch(string[] words)
+    {
+        String basicText = words[0].ToLower();
+        if (OnRequest != null)
+            OnRequest.Invoke(basicText);
+    }
+
+    public void OnStartRecording()
+    {
+        Debug.Log("Recording started");
+    }
+
+    public void OnResult(string sentence)
+    {
+        Debug.Log("Result");
+        Debug.Log(sentence);
+    }
+
+    public void OnComplete(string sentence)
+    {
+        Debug.Log("Complete");
+        Debug.Log(sentence);
+    }
+
+    public void OnHypo(string sentence)
+    {
+        Debug.Log("hypothesis");
+        Debug.Log(sentence);
+    }
+
+    public void OnError(string message)
+    {
+        Debug.Log("Error");
+        Debug.Log(message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
